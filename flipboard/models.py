d00850af071b4476bf9b2e6bb59045e0bda4cb8c
TanFangLong <15387576093@163.com>
2019-03-08 18:18:40 +0800

    create models

---
+++ flipboard/models.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+from flipboard import db
+
+
+class BaseModel(object):
+    """模型基类，为每个模型补充创建时间与更新时间"""
+    create_time = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
+    update_time = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)  # 记录的更新时间
+
+
+# 用户收藏表，建立用户与其收藏新闻多对多的关系
+tb_user_collection = db.Table(
+    "info_user_collection",
+    db.Column("user_id", db.Integer, db.ForeignKey("info_user.id"), primary_key=True),  # 新闻编号
+    db.Column("news_id", db.Integer, db.ForeignKey("info_news.id"), primary_key=True),  # 分类编号
+    db.Column("create_time", db.DateTime, default=datetime.now)  # 收藏创建时间
+)
+# 用户关注表
+tb_user_follows = db.Table(
+    "info_user_fans",
+    db.Column('follower_id', db.Integer, db.ForeignKey('info_user.id'), primary_key=True),  # 粉丝id
+    db.Column('followed_id', db.Integer, db.ForeignKey('info_user.id'), primary_key=True)  # 被关注人的id
+)
+
+
+class User(BaseModel, db.Model):
+    """用户"""
+    __tablename__ = "info_user"
+
+    id = db.Column(db.Integer, primary_key=True)  # 用户编号
+    nick_name = db.Column(db.String(32), unique=True, nullable=False)  # 用户昵称
+    password_hash = db.Column(db.String(128), nullable=False)  # 加密的密码
+    mobile = db.Column(db.String(11), unique=True, nullable=False)  # 手机号
+    avatar_url = db.Column(db.String(256))  # 用户头像路径
+    last_login = db.Column(db.DateTime, default=datetime.now)  # 最后一次登录时间
+    is_admin = db.Column(db.Boolean, default=False)
+    signature = db.Column(db.String(512))  # 用户签名
+    gender = db.Column(  # 订单的状态
+        db.Enum(
+            "MAN",  # 男
+            "WOMAN"  # 女
+        ),
+        default="MAN")
+
+    # 当前用户收藏的所有新闻
+    collection_news = db.relationship("News", secondary=tb_user_collection, lazy="dynamic")  # 用户收藏的新闻
+    # 用户所有的粉丝，添加了反向引用followed，代表用户都关注了哪些人
+    followers = db.relationship('User',
+                                secondary=tb_user_follows,
+                                primaryjoin=id == tb_user_follows.c.followed_id,
+                                secondaryjoin=id == tb_user_follows.c.follower_id,
+                                backref=db.backref('followed', lazy='dynamic'),
+                                lazy='dynamic')
+
+    # 当前用户所发布的新闻
+    news_list = db.relationship('News', backref='user', lazy='dynamic')
+
+
+class Category(BaseModel, db.Model):
+    """新闻分类"""
+    __tablename__ = "info_category"
+
+    id = db.Column(db.Integer, primary_key=True)  # 分类编号
+    name = db.Column(db.String(64), nullable=False)  # 分类名
+    news_list = db.relationship('News', backref='category', lazy='dynamic')
+
+
+class News(BaseModel, db.Model):
+    """新闻"""
+    __tablename__ = "info_news"
+
+    id = db.Column(db.Integer, primary_key=True)  # 新闻编号
+    title = db.Column(db.String(256), nullable=False)  # 新闻标题
+    source = db.Column(db.String(64), nullable=False)  # 新闻来源
+    digest = db.Column(db.String(512), nullable=False)  # 新闻摘要
+    content = db.Column(db.Text, nullable=False)  # 新闻内容
+    clicks = db.Column(db.Integer, default=0)  # 浏览量
+    index_image_url = db.Column(db.String(256))  # 新闻列表图片路径
+    category_id = db.Column(db.Integer, db.ForeignKey("info_category.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("info_user.id"))  # 当前新闻的作者id
+    status = db.Column(db.Integer, default=0)  # 当前新闻状态 如果为0代表审核通过，1代表审核中，-1代表审核不通过
+    reason = db.Column(db.String(256))  # 未通过原因，status = -1 的时候使用
+    # 当前新闻的所有评论
+    comments = db.relationship("Comment", lazy="dynamic")
+
+
+class Comment(BaseModel, db.Model):
+    """评论"""
+    __tablename__ = "info_comment"
+
+    id = db.Column(db.Integer, primary_key=True)  # 评论编号
+    user_id = db.Column(db.Integer, db.ForeignKey("info_user.id"), nullable=False)  # 用户id
+    news_id = db.Column(db.Integer, db.ForeignKey("info_news.id"), nullable=False)  # 新闻id
+    content = db.Column(db.Text, nullable=False)  # 评论内容
+    parent_id = db.Column(db.Integer, db.ForeignKey("info_comment.id"))  # 父评论id
+    parent = db.relationship("Comment", remote_side=[id])  # 自关联
+    like_count = db.Column(db.Integer, default=0)  # 点赞条数
+    
+
+class CommentLike(BaseModel, db.Model):
+    """评论点赞"""
+    __tablename__ = "info_comment_like"
+    comment_id = db.Column("comment_id", db.Integer, db.ForeignKey("info_comment.id"), primary_key=True)  # 评论编号
+    user_id = db.Column("user_id", db.Integer, db.ForeignKey("info_user.id"), primary_key=True)  # 用户编号
